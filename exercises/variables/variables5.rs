c74fca62a1a0119de5baf19a914f60da96a3ef27
Harizo Rajaona <harizo.rajaona@gmail.com>
2021-06-05 13:19:21 +0200

    Current progress

--- exercises/variables/variables5.rs
+++ exercises/variables/variables5.rs
@@ -1,11 +1,10 @@
 // variables5.rs
 // Make me compile! Execute the command `rustlings hint variables5` if you want a hint :)
 
-// I AM NOT DONE
 
 fn main() {
-    let number = "T-H-R-E-E";
-    println!("Spell a Number : {}", number);
-    number = 3;
+    let number_spelling = String::from("T-H-R-E-E");
+    println!("Spell a Number : {}", number_spelling);
+    let number = 3;
     println!("Number plus two is : {}", number + 2);
 }
